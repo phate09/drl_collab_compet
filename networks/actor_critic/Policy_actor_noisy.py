82000bb82bd112f1fcc7cec41b170de46677e8e0
edoardo <phate09@hotmail.it>
2019-08-24 12:02:15 +0100

    added noisy version of the actor and the critic

---
+++ networks/actor_critic/Policy_actor_noisy.py
@@ -0,0 +1,36 @@
+import torch
+import torchtest
+from torch import nn as nn
+from torch.nn import functional as F
+from networks.NoisyLinear import NoisyLinear
+
+
+class Policy_actor(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc1 = NoisyLinear(self.input_dim, 64)
+        self.bn1 = nn.BatchNorm1d(64)
+        self.fc2 = NoisyLinear(64, 64)
+        self.bn2 = nn.BatchNorm1d(64)
+        self.fc3 = NoisyLinear(64, self.output_dim)
+        self.bn3 = nn.BatchNorm1d(self.output_dim)
+
+    def forward(self, x):
+        output: torch.Tensor = F.relu(self.bn1(self.fc1(x)))
+        output: torch.Tensor = F.relu(self.bn2(self.fc2(output)))
+        output: torch.Tensor = torch.tanh(self.bn3(self.fc3(output)))
+        # output: torch.Tensor = output.mul(5.0)
+        return output
+
+    def test(self, device='cpu'):
+        self.eval()
+        input = torch.randn(10, self.input_dim, requires_grad=False)
+        targets = torch.rand(10, self.output_dim, requires_grad=False)
+        torchtest.test_suite(self, F.mse_loss, torch.optim.Adam(self.parameters()), batch=[input, targets], test_vars_change=True, test_inf_vals=True, test_nan_vals=True, device=device)
+        print('All tests passed')
+
+if __name__ == '__main__':
+    model = Policy_actor(33,4)
+    model.test()
