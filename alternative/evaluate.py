7e63a2f30c93d7990e867f4eab10de825c71d1c2
edoardo <phate09@hotmail.it>
2019-09-03 22:57:47 +0100

    modified evaluate class

--- alternative/evaluate.py
+++ alternative/evaluate.py
@@ -16,6 +16,8 @@ from munch import Munch, DefaultMunch
 from tensorboardX import SummaryWriter
 import random
 
+from utility.ReplayMemory import ExperienceReplayMemory
+
 '''
 Begin help functions and variables
 '''
@@ -35,11 +37,11 @@ if __name__ == '__main__':
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
-    worker_id = 1
+    worker_id = 10
     print(f'Worker_id={worker_id}')
-    env = UnityEnvironment("../environment/Tennis_Linux/Tennis.x86_64", worker_id=worker_id, seed=seed, no_graphics=True)
+    env = UnityEnvironment("../environment/Tennis_Linux/Tennis.x86_64", worker_id=worker_id, seed=seed, no_graphics=False)
     brain = env.brains[env.brain_names[0]]
-    env_info = env.reset(train_mode=True)[env.brain_names[0]]
+    env_info = env.reset(train_mode=False)[env.brain_names[0]]
     n_agents = len(env_info.agents)
     print('Number of agents:', n_agents)
     action_size = brain.vector_action_space_size
@@ -48,17 +50,10 @@ if __name__ == '__main__':
     action_type = brain.vector_action_space_type
     comment = f"TD3 Unity Tennis"
     # actor1.test(device)
-    optimizer_actor_fn = lambda actor: optim.Adam(actor.parameters(), lr=1e-4)
-    optimizer_critic_fn = lambda critic: optim.Adam(critic.parameters(), lr=1e-4)
-    ending_condition = lambda result: result['mean'] >= 300.0
-    log_dir = os.path.join('../runs', current_time + '_' + comment)
-    os.mkdir(log_dir)
-    print(f"logging to {log_dir}")
-    writer = SummaryWriter(log_dir=log_dir)
-
+    rand_seed = 0
     config = DefaultMunch()
     config.seed = seed
-    config.n_episodes = 40000
+    config.n_episodes = 10
     config.max_t = 1000
     config.buffer_size = 100000  # replay buffer size
     config.batch_size = 200  # minibatch size
@@ -66,11 +61,14 @@ if __name__ == '__main__':
     config.tau = 0.001  # for soft update of targt params
     config.lr_actor = 0.0001  # learning rate of the actor
     config.lr_critic = 0.001  # learning rate of the critic
+    config.n_agents = n_agents
+    config.state_size = state_size * state_multiplier
+    config.action_size = action_size
+    config.learn_start = 3000
+    config.max_action = 1
+    config.memory = ExperienceReplayMemory(config.buffer_size, rand_seed)
     config.update_every = 2  # steps to update
     config.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    config_file = open(os.path.join(log_dir, "config.json"), "w+")
-    config_file.write(json.dumps(json.loads(jsonpickle.encode(config, unpicklable=False, max_depth=1)), indent=4, sort_keys=True))
-    config_file.close()
     rand_seed = 0
     scores = []
     scores_std = []
@@ -78,24 +76,22 @@ if __name__ == '__main__':
     scores_window = deque(maxlen=100)  # last 100 scores
 
     agent = MultiAgent(config, state_size * state_multiplier, action_size, rand_seed)
-
-    print('\nTRAINING:')
+    agent.load("../runs/Sep03_20-02-15_TD3 Unity Tennis/checkpoint_success.pth")
+    print('\nEVALUATING:')
     # start the training
     global_steps = 0
     noise_scheduler = config.noise_scheduler
     for i_episode in range(config.n_episodes):
-        env_info = env.reset(train_mode=True)[env.brain_names[0]]  # reset the environment
+        env_info = env.reset(train_mode=False)[env.brain_names[0]]  # reset the environment
         states = torch.tensor(env_info.vector_observations, dtype=torch.float, device=device)  # get the current state
         score = 0
         # noise_magnitude = noise_scheduler.get(global_steps)
         for i in range(config.max_t):
-            actions = agent.act(states, add_noise=True)
+            actions = agent.act(states, add_noise=False)
             env_info = env.step(actions.cpu().numpy())[env.brain_names[0]]
             next_states = torch.tensor(env_info.vector_observations, dtype=torch.float, device=device)  # get the next state
             rewards = torch.tensor(env_info.rewards, dtype=torch.float, device=device).unsqueeze(dim=1)  # get the reward
             dones = torch.tensor(env_info.local_done, dtype=torch.uint8, device=device).unsqueeze(dim=1)  # see if episode has finished
-            # next_states, rewards, dones = env.step(actions)
-            agent.step(states, actions, rewards, next_states, dones)
             global_steps += 1
             score += torch.max(rewards).item()
             states = next_states
@@ -105,20 +101,13 @@ if __name__ == '__main__':
         scores_window.append(score)
         scores_std.append(np.std(scores_window))
         scores_avg.append(np.mean(scores_window))
-        writer.add_scalar('data/score', score, i_episode)
-        writer.add_scalar('data/score_average', np.mean(scores_window), i_episode)
-        writer.add_scalar('data/score_max', np.max(scores_window), i_episode)
-        writer.add_scalar('data/score_min', np.min(scores_window), i_episode)
-        writer.add_scalar('data/score_std', np.std(scores_window), i_episode)
-        s_msg = '\rEpisode {}\tAverage Score: {:.3f}\tσ: {:.3f}\tScore: {:.3f}'
-        print(s_msg.format(i_episode, np.mean(scores_window), np.std(scores_window), np.max(score)), end="")
-        if i_episode % 100 == 0:
-            print(s_msg.format(i_episode, np.mean(scores_window), np.std(scores_window), np.max(score)))
-            # agent.save(os.path.join(log_dir, f"checkpoint_{i_episode}.pth"), i_episode)
-        if np.mean(scores_window) >= 0.5:
-            SOLVED = True
-            s_msg = '\n\nEnvironment solved in {:d} episodes!\tAverage Score: {:.3f}\tσ: {:.3f}'
-            print(s_msg.format(i_episode, np.mean(scores_window), np.std(scores_window)))
-            # agent.save(os.path.join(log_dir, f"checkpoint_success.pth"), i_episode)
-            break
+        s_msg = '\rEpisode {}\tAverage Score: {:.3f}\tσ: {:.3f}\tStep: {:.3f}'
+        print(s_msg.format(i_episode, np.mean(scores_window), np.std(scores_window), global_steps), end="")
+        # if i_episode % 100 == 0 and i_episode != 0:
+        #     print(s_msg.format(i_episode, np.mean(scores_window), np.std(scores_window), global_steps))
+        # if np.mean(scores_window) >= 0.9:
+        #     SOLVED = True
+        #     s_msg = '\n\nEnvironment solved in {:d} episodes!\tAverage Score: {:.3f}\tσ: {:.3f}'
+        #     print(s_msg.format(i_episode, np.mean(scores_window), np.std(scores_window)))
+        #     break
     print("Finished.")
